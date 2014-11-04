de9f17ff336b9fc3c8a1383b33e294dc0c7975c1
stairc <cameronstair@gmail.com>
2014-11-04 15:48:04 -0500

    Edited AppDataContainer (originally Application)
    Reformatted it and added it to correct directory in the project

---
+++ AppMap/AppMap/AppDataContainer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AppMap
+{
+    public class AppDataContainer
+    {
+        private string author;
+        private string title;
+        private string description;
+
+        public AppDataContainer()
+        {
+            author = "Default";
+            title = "Default";
+            description = "Default";
+        }
+        public AppDataContainer(string title, string author, string description)
+        {
+            this.author = author;
+            this.title = title;
+            this.description = description;
+        }
+
+        public void setAuthor(string author)
+        {
+            this.author = author;
+        }
+
+        public void setTitle(string title)
+        {
+            this.title = title;
+        }
+
+        public void setDescription(string description)
+        {
+            this.description = description;
+        }
+
+        public string getAuthor()
+        {
+            return author;
+        }
+
+        public string getTitle()
+        {
+            return title;
+        }
+
+        public string getDescription()
+        {
+            return description;
+        }
+    }
+}
