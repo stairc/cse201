640f112a8791c2827b9c2e8633f51a8acbfe5416
dazeycm <dazeycm@miamioh.edu>
2014-11-05 16:53:41 -0500

    Added db and class to interact with it

---
+++ AppMap/AppMap/DBInteraction.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AppMap
+{
+    public class DBInteraction
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source = C:\Users\Craig\Desktop\201\cse201\AppMap\AppMap; Version = 3;";
+            SqlConnection dbConnection = new SqlConnection(connectionString);
+            dbConnection.Open();
+
+            string query = "SELECT * FROM App";
+            SqlCommand queryCommand = new SqlCommand(query, dbConnection);
+            SqlDataReader queryReader = queryCommand.ExecuteReader();
+            DataTable data = new DataTable();
+
+            data.Load(queryReader);
+
+            int firstCoupleRows = 3;
+
+            for (int i = 0; i < firstCoupleRows; i++)
+            {
+                String rowText = string.Empty;
+                foreach (DataColumn column in data.Columns)
+                {
+                    rowText += data.Rows[i][column.ColumnName] + " | ";
+                }
+                Console.WriteLine(rowText);
+            }
+
+            dbConnection.Close();
+
+
+        }
+    }
+}
