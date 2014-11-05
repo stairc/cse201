834da9d8071ba655729e046660cc1a1d2e00156a
Thomas Ricketts <ricketta@miamioh.edu>
2014-11-04 19:27:25 -0500

    Added new data points from meeting 2.

--- AppMap/AppMap/AppDataContainer.cs
+++ AppMap/AppMap/AppDataContainer.cs
@@ -10,18 +10,31 @@ namespace AppMap
         private string author;
         private string title;
         private string description;
+        private string url_to_product;
+        private int rating;
+        private double cost;
+        private int store; // Stores will be assigned numerical integers. 
 
         public AppDataContainer()
         {
             author = "Default";
             title = "Default";
             description = "Default";
+            url_to_product = "Default";
+            rating = 0;
+            cost = 0.00;
+            store = NULL;
         }
-        public AppDataContainer(string title, string author, string description)
+        public AppDataContainer(string title, string author, string description, 
+                string url, int rating, double cost, int store)
         {
             this.author = author;
             this.title = title;
             this.description = description;
+            this.url_to_product = url;
+            this.rating = rating;
+            this.cost = cost;
+            this.store = store; 
         }
 
         public void setAuthor(string author)
@@ -39,6 +52,24 @@ namespace AppMap
             this.description = description;
         }
 
+        public void setURL(string url) {
+            this.url_to_product = url;
+        }
+
+        public void setRating(int rating)
+        {
+            this.rating = rating;
+        }
+
+        public void set(double cost)
+        {
+            this.cost = cost;
+        }
+
+        public void set()
+        {
+            this.store = store;
+        }
         public string getAuthor()
         {
             return author;
@@ -52,6 +83,22 @@ namespace AppMap
         public string getDescription()
         {
             return description;
+        }
+
+        public string getURL() {
+            return url_to_product;
+        }
+
+        public int getRating() {
+            return rating;
+        }
+
+        public double getCost() {
+            return cost;
+        }
+
+        public int getStore() {
+            return store;
         }
     }
 }
