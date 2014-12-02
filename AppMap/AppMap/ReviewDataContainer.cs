bc1315aba524e76cc1d7ae43eaa7a43645418baf
unknown <bowdlesa@EAS-BEN001-A6.it.muohio.edu>
2014-12-02 18:16:50 -0500

    Made ReviewDataContainer.

---
+++ AppMap/AppMap/ReviewDataContainer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AppMap
+{
+    public class ReviewDataContainer
+    {
+        private string name;
+        private double rating;
+        private string comment;
+
+        public ReviewDataContainer()
+        {
+
+        }
+
+        public ReviewDataContainer(string name, double rating, string comment)
+        {
+            this.name = name;
+            this.rating = rating;
+            this.comment = comment;
+        }
+
+        protected double getName()
+        { return name; }
+        protected double getRating()
+        { return rating; }
+        protected double getComment()
+        { return comment; }
+
+        protected void setname(string name)
+        { this.name = name; }
+        protected void setRating(double rating)
+        { this.rating = rating; }
+        protected void setComment(string comment)
+        { this.comment = comment; }
+    }
+}
